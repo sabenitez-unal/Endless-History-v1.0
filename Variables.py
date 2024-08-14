412b75e91fea175796365a048d33b6a1895497cb
sabenitez-unal <167267294+sabenitez-unal@users.noreply.github.com>
2024-08-13 22:14:39 -0500

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -2,21 +2,21 @@ import pygame
 
 pygame.init()
 
-# Colores
+# Colores.
 VERDE = (0, 255, 0)
 ROJO = (255, 0, 0)
 white = (255, 255, 255)
 
-# Nivel
+# Nivel.
 ARMA_SEGUN_LEVEL = 1
 
-# Asignando colores a los textos
+# Asignando colores a los textos.
 text_color = (0, 0, 0)
 txt_color_play = (255, 255, 255)
 txt_color_go = (255, 255, 255)
 txt_color_colission = (245, 176, 65)
 
-# Creando fuentes de textos de opciones y título
+# Creando fuentes de textos de opciones y título y mensaje de victoria.
 text_titles = pygame.font.Font('fonts/PixelifySans-Regular.ttf', 60)
 text_options = pygame.font.Font('fonts/PixelifySans-Regular.ttf', 35)
 text_in_game = pygame.font.Font('fonts/PixelifySans-Regular.ttf', 25)
